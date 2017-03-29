df731c5de69ec3cc3e6e4e1566c103d89e484d30
Abdullah Basit <abdullah.basit@hotmail.com>
2017-03-29 12:48:48 +0500

    AngularJs Jwt handler boilerplate

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,6 +6,9 @@
  */
 
 require('./bootstrap');
+require('angular');
+require('angular-ui-router');
+require('satellizer');
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -13,8 +16,60 @@ require('./bootstrap');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+var gorillaApp = angular.module('gorillaApp', ['ui.router', 'satellizer']);
 
-const app = new Vue({
-    el: '#app'
-});
+gorillaApp.config(['$stateProvider', '$urlRouterProvider', '$authProvider', function($stateProvider, $urlRouterProvider, $authProvider) {
+    // Satellizer configuration that specifies which API
+            // route the JWT should be retrieved from
+            $authProvider.loginUrl = '/api/authenticate';
+
+            // Redirect to the auth state if any other states
+            // are requested other than users
+            $urlRouterProvider.otherwise('/auth');
+            
+            $stateProvider
+                .state('auth', {
+                    url: '/auth',
+                    templateUrl: '../views/authView.html',
+                    controller: 'AuthController as auth'
+                })
+                .state('users', {
+                    url: '/users',
+                    templateUrl: '../views/userView.html',
+                    controller: 'UserController as user'
+                });
+}]);
+
+gorillaApp.controller('AuthController', ['$auth', '$state',function($auth, $state) {
+    var vm = this;
+
+    vm.login = function() {
+        var credentials =  {
+            email: vm.email,
+            password: vm.password
+        }
+
+        $auth.login(credentials).then(function(data) {
+            //if Login success redirect to users $state
+            $state.go('users', {});
+        });
+    }
+}]);
+
+gorillaApp.controller('UserController', ['$http', function($http) {
+
+    var vm = this;
+
+    vm.users = {};
+    vm.error = '';
+
+    vm.getUsers = function() {
+        $http.get('api/authenticate').then(function(data) {
+            vm.users = data.data;
+            //console.log(data.data);
+        }, function(error) {
+            vm.error = error;
+        });
+    }
+
+}]);
---
+++ resources/views/index.php
@@ -0,0 +1,25 @@
+<!-- resources/views/index.php -->
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Angular-Laravel Authentication</title>
+        <link rel="stylesheet" href="css/app.css">
+    </head>
+    <body ng-app="gorillaApp">
+
+        <div class="container">
+            <div ui-view></div>
+        </div>        
+
+    </body>
+
+     <!--Application Dependencies 
+    <script src="node_modules/angular/angular.js"></script>
+    <script src="node_modules/angular-ui-router/build/angular-ui-router.js"></script>
+    <script src="node_modules/satellizer/satellizer.js"></script>-->
+
+    <!-- Application Scripts -->
+    <script src="js/app.js"></script>
+</html>
